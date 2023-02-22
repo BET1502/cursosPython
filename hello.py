dcb948c6cf9a98d3a2b93ac7e49a28f51b292237
BET1502 <estevez1502@hotmail.com>
2023-02-22 13:55:38 +0100

    Moure DEV - Hola Mundo

---
+++ hello.py
@@ -0,0 +1,24 @@
+# Esto es un comentario: Hola mundo
+# Nuestro hola mundo en Python
+print("Hola mundo")
+print('Hola mundo')
+
+"""
+Este es un
+comentario
+en varias líneas
+"""
+
+'''
+Este también es un
+comentario
+en varias líneas
+'''
+
+#Consultar el tipo de dato
+print(type("Soy un dato str")) # Tipo 'str'
+print(type(5)) # Tipo 'str'
+print(type(1.5)) # Tipo 'float'
+print(type(1 + 2j)) # Tipo 'complex'
+print(type(True)) # Tipo 'bool'
+
