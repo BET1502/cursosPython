98d260ad3ce208ae333c0cdda22b1cd76401ce1e
BET1502 <estevez1502@hotmail.com>
2023-03-06 11:08:38 +0100

    Moure DEV - Operators

---
+++ 02_operators.py
@@ -0,0 +1,47 @@
+### Operadores Aritméticos ###
+
+print(3 + 4)
+print(3 - 4)
+print(3 * 4)
+print(3 / 3)
+print(10 % 3)
+print(10 // 3) # hace que la división acabe aproximada a un número entero
+print(2 ** 3) # esto significa 2 elevado a 3
+print(2 ** 3 + 3 - 7 / 1 // 4) 
+
+print("Hola " + "Python " + "¿Qué tal?")
+print("Hola " + str(5))
+print("Hola " + 5)
+print("Hola " * (2 ** 3))
+
+my_float = 2.5 * 2
+print("Hola " * int(my_float))
+
+### Operadores comparativos ###
+
+print(3 > 4)
+print(3 < 4)
+print(3 >= 4)
+print(3 <= 4)
+print(3 == 4)
+print(3 != 4)
+
+print("Hola" > "Python")
+print("Hola" < "Python")
+print("aaaa" >= "abaa") # Ordenación alfabética por ASCII
+print(len("aaaa") >= len("abaa")) # Cuenta caracteres
+print("Hola" <= "Python")
+print("Hola" == "Hola")
+print("Hola" != "Python")
+
+### Operadores lógicos ###
+
+print(3 > 4 and "Hola" > "Python")
+print(3 > 4 and "Hola" > "Python")
+print(3 > 4 and "Hola" < "Python")
+print(3 < 4 or ("Hola" > "Python" and 4 == 4))
+print(not (3 > 4))
+
+
+
+
